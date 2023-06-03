d5a2400cd7cafc2d40a537d69201502104b4f929
Vasiliy Pavlov <vasiliy.pavlov@solisoft.group>
2023-06-03 13:36:46 +0300

    Add Cors

--- OOP_Microservices/Program.cs
+++ OOP_Microservices/Program.cs
@@ -28,6 +28,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
